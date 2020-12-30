6af82ff284a5e458ee46d946e8ee7bd28b4c8ab6
zhangruiqi <zrqjob666@163.com>
2020-12-30 15:47:02 +0800

    add

---
+++ src/pages/pc/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="pc-con">
+    <div class="container">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    components: {},
+    data(){
+      return {}
+    },
+    created() {},
+    methods: {}
+  }
+
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
