e442afdee73dd569451221ade513de6ec2d71ac0
zhangruiqi <zrqjob666@163.com>
2020-12-31 10:53:43 +0800

    add

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -5,7 +5,7 @@
       username="gh_3a4e33c5ebb6"
     >
       <script type="text/wxtag-template">
-        建设通出品
+        <a>建设通出品</a>
       </script>
     </wx-open-launch-weapp>
     <a>数据截止日期：2020.12.15</a>
